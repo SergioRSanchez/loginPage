49a1196d272d97e6a92c1f3a2a66fc73693f2555
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-03-23 21:13:31 -0300

    show and hide password function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 import { ReactComponent as Logo } from './assets/logo.svg';
 import { ReactComponent as EyeOff } from './assets/eye-off.svg';
+import { ReactComponent as Eye } from './assets/eye.svg';
 
 function App() {
+  const [ showPassword, setShowPassword ] = useState(false);
+
+  function handleShowPassword() {
+    if (showPassword === false) {
+      setShowPassword(true);
+    } else {
+      setShowPassword(false);
+    }
+  };
+
   return (
     <div className='font-["Titillium_Web"] bg-[url("/src/assets/background.png")] bg-cover flex flex-col sm:flex-row sm:bg-none h-screen'>
       <div className='flex-1 bg-[#F8FAFC;] rounded-xl mx-3 my-3 sm:mx-0 sm:my-0 flex justify-center sm:justify-start'>
@@ -15,18 +28,22 @@ function App() {
             </div>
             <div className='max-w-[384px] flex flex-col gap-8 pb-2'>
               <div className='flex flex-col gap-4'>
-                <form className='flex flex-col gap-2'>
+                <form className='flex flex-col gap-2 relative'>
                   <label for='email' className='text-sm font-semibold'>E-mail</label>
                   <input required id='email' name='email' type="email" className='border-[1px] border-[#E2E8F0] hover:border-[#7C3AED] focus:border-[#7C3AED] outline-0 outline-[#7C3AED] rounded-[4px] w-auto mr-3 sm:w-[384px] px-4 py-3 text-sm' placeholder='Digite seu e-mail'/>
                   <div className='flex justify-between'>
                     <label for='password' className='text-sm font-semibold'>Senha</label>
                     <button><span className='text-sm font-semibold text-[#7C3AED] mr-4 sm:mr-0'>Esqueceu sua senha?</span></button>
                   </div>
-                  <form className='flex justify-between bg-white border-[1px] border-[#E2E8F0] hover:border-[#7C3AED] focus-within:border-[#7C3AED] focus:border-[#7C3AED] outline-0 outline-[#7C3AED] rounded-[4px] w-auto mr-3 sm:w-[384px] px-4 py-3 text-sm'>
-                    <input required id='password' name='password' type="password" className='w-[300px] sm:w-[330px] outline-none' placeholder='Digite sua senha'/>
-                    <button><EyeOff /></button>
-                  </form>
-                  <input type="submit" value="Entrar" className='mr-3 mt-8 sm:mr-0 bg-[#7C3AED] hover:bg-[#9F67FF] py-4 px-6 rounded-[4px] font-bold text-white'/>
+                  <input required id='password' name='password' type={showPassword ? 'text' : 'password'} className='border-[1px] border-[#E2E8F0] hover:border-[#7C3AED] focus:border-[#7C3AED] outline-0 outline-[#7C3AED] rounded-[4px] w-auto mr-3 sm:w-[384px] px-4 py-3 text-sm' placeholder='Digite sua senha'/>
+                  {
+                    showPassword
+                    ?
+                    <Eye onClick={handleShowPassword} className='absolute right-5 top-[125px] cursor-pointer' />
+                    :
+                    <EyeOff onClick={handleShowPassword} className='absolute right-5 top-[125px] cursor-pointer' />
+                  }
+                  <input type="submit" value="Entrar" className='mr-3 mt-8 sm:mr-0 bg-[#7C3AED] hover:bg-[#9F67FF] py-4 px-6 rounded-[4px] font-bold text-white cursor-pointer'/>
                 </form>
               </div>
               
